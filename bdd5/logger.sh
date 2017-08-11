62d5e35ccc5a7549b0443123554411b171f7b90a
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-08-11 16:42:01 -0700

    automated commit

--- bdd5/logger.sh
+++ bdd5/logger.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-LOGDATE=$(date +%Y-%m[%b]-%d_%H:%M:%S)
-cd /home/sauhaarda/working-directory/computerlogs/bdd5 # insert path here
+LOGDATE=$(date +%Y-%m[%b]-%d_%H:%M:%S)".md"
+# cd /home/sauhaarda/working-directory/computerlogs/bdd5 # insert path here
 
+echo "# GPU Process top Output" >> $LOGDATE
+echo "\`\`\`" >> $LOGDATE
 PIDS=$(nvidia-smi | awk '$2$3 ~ /^[0-9]+$/{print $3}')
 # Print GPU Users
 echo "  PID USER      PR  NI    VIRT    RES    SHR S  %CPU %MEM     TIME+ COMMAND" >> $LOGDATE
 while read -r line; do
 	top -n1 -b -p "$line" | tail -n 1 >> $LOGDATE
 done <<< "$PIDS"
+echo "\`\`\`" >> $LOGDATE
 
 # Print GPU Statistics
+echo "# NVIDIA SMI Output" >> $LOGDATE
+echo "\`\`\`" >> $LOGDATE
 nvidia-smi >> $LOGDATE
+echo "\`\`\`" >> $LOGDATE
+echo "# Detailed GPU Statistics" >> $LOGDATE
+echo "\`\`\`" >> $LOGDATE
 nvidia-smi -q -d UTILIZATION >> $LOGDATE
+echo "\`\`\`" >> $LOGDATE
 
 # Push to GitHub
 git pull
